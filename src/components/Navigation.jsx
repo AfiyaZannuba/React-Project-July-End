c8514d33d231395986e251772e07e999cbc65c37
AfiyaZannuba <afiya.zannubaa@gmail.com>
2023-07-29 18:30:03 +0530

    first commit of building a brand page from figma file

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+return(
+    <nav>
+            <div className="logo">
+                <img src="/images/shoe_image.png" alt="brand_logo" />
+            </div>
+            <ul>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Location</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+
+            <button>Login</button>
+        </nav>
+)
+} 
+
+export default Navigation;
